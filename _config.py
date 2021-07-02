f681e9568b6d11eba13d0f9e96101a435e88b94b
Richard Bocchinfuso <rbocchinfuso@gmail.com>
2021-07-02 10:45:48 -0400

    July 2, 2021 10:45 AM

--- _config.py
+++ _config.py
@@ -8,7 +8,7 @@ __copyright__ = "Copyright 2021, Snowmail ServieNow Email Helper."
 __credits__ = ["Rich Bocchinfuso"]
 
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __maintainer__ = "Rich Bocchinfuso"
 __email__ = "rbocchinfuso@gmail.com"
 __status__ = "Dev"
